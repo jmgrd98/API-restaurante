64d46af3378cd075ce2a076209fe3e353391a998
João Marcelo Dantas <jmgrd98@gmail.com>
2023-04-08 11:15:41 -0300

    hello world

--- index.ts
+++ index.ts
@@ -9,3 +9,13 @@ app.use(
 
 app.use(express.json());
 
+app.get('/', (req: any, res: any) => {
+
+    res.json({message: "Hello, World!"})
+});
+
+app.post('/auth/login', (req: any, res: any) => {
+
+});
+
+app.listen(3000);
