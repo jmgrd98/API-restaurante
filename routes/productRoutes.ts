78f6bcedde14d78e94a942058bd83318899750ba
João Marcelo Dantas <jmgrd98@gmail.com>
2023-04-08 15:55:00 -0300

    Update productRoutes.ts

---
+++ routes/productRoutes.ts
@@ -0,0 +1,23 @@
+var express = require('express');
+const router = express.Router();
+const category = require('../models/Category')
+
+router.get('/product', (req: any, res: any) => {
+
+});
+
+router.get('/product/:id', (req: any, res: any) => {
+
+});
+
+router.post('/product/', (req: any, res: any) => {
+
+});
+
+router.patch('/product/:id', (req: any, res: any) => {
+
+});
+
+router.delete('/product/:id', (req: any, res: any) => {
+
+});
