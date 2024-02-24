c01f04ebacce584ffe76b332f07bf67f3fe675f3
João Marcelo Dantas <jmgrd98@gmail.com>
2024-02-24 20:15:33 -0300

    commit

--- index.ts
+++ index.ts
@@ -2,13 +2,9 @@ var express = require('express');
 const app = express();
 const mongo = require('mongoose');
 const cors = require('cors');
-import swaggerUi from 'swagger-ui-express';
-import swaggerDocs from './swagger.json';
-import * as dotenv from 'dotenv';
-
 app.use(cors());
 
-dotenv.config();
+// dotenv.config();
 const dbUser = process.env.DB_USER;
 const dbPassword = process.env.DB_PASSWORD;
 
@@ -28,7 +24,7 @@ app.use('/auth', adminRoutes);
 app.use('/category', categoryRoutes);
 app.use('/product', productRoutes);
 
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(require("./swagger.json")));
+// app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(require("./swagger.json")));
 
 mongo.connect(`mongodb+srv://${dbUser}:${dbPassword}@apicluster0.y7vkluv.mongodb.net/?retryWrites=true&w=majority`)
     .then(() => {
